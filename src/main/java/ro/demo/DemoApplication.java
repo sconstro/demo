ca4f18f42cf20d73921f57d7bc019e9a1ddb267e
sconstro@yahoo.com <sconstro@yahoo.com>
2016-06-14 10:29:53 +0300

    demo

--- src/main/java/ro/demo/DemoApplication.java
+++ src/main/java/ro/demo/DemoApplication.java
@@ -2,11 +2,16 @@ package ro.demo;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication(scanBasePackages = "ro.demo")
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration.class
+})
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
